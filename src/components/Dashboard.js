d337435c9c6883e928edee1b7313d23de9c5ec24
Dumeni Vincenz <dvincenz@hsr.ch>
2017-04-13 15:56:09 +0200

    initial Dashboard

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -1,6 +1,8 @@
 // @flow
 
 import React from 'react'
+import { Container, Segment, Form, Input, Button, Icon, Grid } from 'semantic-ui-react'
+
 
 /*
   Use the api functions to call the API server. For example, the transactions
@@ -19,17 +21,69 @@ export type Props = {
 }
 
 class Dashboard extends React.Component {
-  
+
   props: Props
-  
-  render() {    
+
+  render() {
 
     return (
-      <div>
-        This is the dashboard
-      </div>
+      <Container>
+        <Segment style={{ marginTop: 10 }}>
+
+          <Grid columns={2} divided>
+            <Grid.Row>
+              <Grid.Column>
+                <NewPayment />
+              </Grid.Column>
+              <Grid.Column>
+                <PayHistory />
+              </Grid.Column>
+            </Grid.Row>
+          </Grid>
+        </Segment>
+      </Container>
     )
   }
+}
+
+class PayHistory extends React.Component {
+
+  render() {
+    return (
+      <div>
+        This is a payment history
+      </div>
+    );
+  }
+}
+
+class NewPayment extends React.Component {
+  state: {
+    from: string,
+    to: string,
+  }
+  state = {
+    from: "",
+    to: "",
+  }
+  handleSubmit() {
+    console.log("just submited form")
+  }
+  render() {
+    return (
+      <Form className='attached fluid segment'>
+        <Form.Field>
+          <Input label="Von" placeholder='10000002' value={this.state.from} />
+        </Form.Field>
+        <Form.Field>
+          <Input label="zu" placeholder='Konto Nr' value={this.state.to} />
+        </Form.Field>
+        <Button onClick={this.handleSubmit}>Log-in</Button>
+      </Form>
+
+    )
+  }
+
 }
 
 export default Dashboard
